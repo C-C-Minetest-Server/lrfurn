1a15f7015467f0cb8b31e773c1ca196d3158b723
1F616EMO <root@1f616emo.xyz>
2024-03-22 19:26:22 +0800

    look yaw and pitch

--- init.lua
+++ init.lua
@@ -7,19 +7,33 @@
 ]]
 
 local S = minetest.get_translator("lrfurn")
+local pi = math.pi
 
 lrfurn = {}
 
+-- The facedir of the node is not that of the player
+-- So we need our own converion table
+local param2_to_horizontal = {
+    [0] = pi * 3 / 2,
+    [1] = pi,
+    [2] = pi / 2,
+    [3] = 0
+}
+
+-- TODO: player_api sit
 function lrfurn.sofa_click(pos, node, clicker)
     if not clicker:is_player() then
         return
     end
     pos.y = pos.y - 0.5
     clicker:set_pos(pos)
+
+    clicker:set_look_horizontal(param2_to_horizontal[node.param2 % 4])
+    clicker:set_look_vertical(pi * 10 / 180)
 end
 
-dofile(minetest.get_modpath("lrfurn").."/longsofas.lua")
-dofile(minetest.get_modpath("lrfurn").."/sofas.lua")
-dofile(minetest.get_modpath("lrfurn").."/armchairs.lua")
-dofile(minetest.get_modpath("lrfurn").."/coffeetable.lua")
-dofile(minetest.get_modpath("lrfurn").."/endtable.lua")
+dofile(minetest.get_modpath("lrfurn") .. "/longsofas.lua")
+dofile(minetest.get_modpath("lrfurn") .. "/sofas.lua")
+dofile(minetest.get_modpath("lrfurn") .. "/armchairs.lua")
+dofile(minetest.get_modpath("lrfurn") .. "/coffeetable.lua")
+dofile(minetest.get_modpath("lrfurn") .. "/endtable.lua")
